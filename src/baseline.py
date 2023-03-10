d326d4898ba9ffc5c3bfdac332192c2574e395ae
srgelinas <77132716+srgelinas@users.noreply.github.com>
2023-03-10 13:13:07 -0800

    updated baseline.py

--- src/baseline.py
+++ src/baseline.py
@@ -12,10 +12,10 @@ def evaluate_baseline(data):
     Helper function split input DataFrame into training/testing sets,
     fit SVM model, and evlauate performance on the testing set.
     """
-    # splite data into train/test sets
+    # split data into train/test sets
     df = pd.DataFrame(data)
-    X_train, y_train = df[df['is_train'] == '1'].drop(columns=['vid', 'label', 'is_test', 'is_train']), df[df['is_train']=='1']['label']
-    X_test, y_test = df[df['is_test'] == '1'].drop(columns=['vid', 'label', 'is_train', 'is_test']), df[df['is_test']=='1']['label']
+    X_train, y_train = df[df['is_train'] == 1.0].drop(columns=['vid', 'label', 'is_test', 'is_train']), df[df['is_train']==1.0]['label']
+    X_test, y_test = df[df['is_test'] == 1.0].drop(columns=['vid', 'label', 'is_train', 'is_test']), df[df['is_test']==1.0]['label']
     # fit SVM model to training data
     np.random.seed(42)
     model = SVC(gamma='auto').fit(X_train, y_train)
@@ -29,10 +29,10 @@ def evaluate_KNN(data):
     Helper function split input DataFrame into training/testing sets,
     fit KNN model, and evlauate performance on the testing set.
     """
-    # splite data into train/test sets
+    # split data into train/test sets
     df = pd.DataFrame(data)
-    X_train, y_train = df[df['is_train'] == '1'].drop(columns=['vid', 'label', 'is_test', 'is_train']), df[df['is_train']=='1']['label']
-    X_test, y_test = df[df['is_test'] == '1'].drop(columns=['vid', 'label', 'is_train', 'is_test']), df[df['is_test']=='1']['label']
+    X_train, y_train = df[df['is_train'] == 1.0].drop(columns=['vid', 'label', 'is_test', 'is_train']), df[df['is_train']==1.0]['label']
+    X_test, y_test = df[df['is_test'] == 1.0].drop(columns=['vid', 'label', 'is_train', 'is_test']), df[df['is_test']==1.0]['label']
     # fit KNN model (k = 3) to training data
     np.random.seed(42)
     model = KNeighborsClassifier(n_neighbors=3).fit(X_train, y_train)
@@ -47,11 +47,11 @@ def evaluate_xgb(data):
     Helper function split input DataFrame into training/testing sets,
     fit XGBoost model, and evlauate performance on the testing set.
     """
-    # splite data into train/test sets
+    # split data into train/test sets
     df = pd.DataFrame(data).astype(float)
     X_train, y_train = df[df['is_train'] == 1.0].drop(columns=['vid', 'label', 'is_test', 'is_train']), df[df['is_train']==1.0]['label']
     X_test, y_test = df[df['is_test'] == 1.0].drop(columns=['vid', 'label', 'is_train', 'is_test']), df[df['is_test']==1.0]['label']
-    # fit KNN model (k = 3) to training data
+    # fit xgboost to training data
     np.random.seed(42)
     model = xgb.XGBClassifier(n_estimators=100, learning_rate=0.001, objective='binary:logistic').fit(X_train,y_train)
     preds = model.predict(X_test)
